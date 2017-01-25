6b8c45373eaedce6af436fe4503ddb76e3d8defb
Carlos Valiente <carlos@pepelabs.net>
2017-01-25 19:23:54 +0000

    Chapter 16.

--- 16-functor/test/exercises-chapter.hs
+++ 16-functor/test/exercises-chapter.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module Main where
 
 import Test.Hspec
@@ -158,6 +160,192 @@ quantIdentity = functorIdentity
 quantComposition :: Quant Int Int-> Fun Int Int -> Fun Int Int -> Bool
 quantComposition = functorComposition
 
+--- Flip f a b
+
+newtype Flip f a b =
+    Flip (f b a)
+    deriving (Eq, Show)
+
+newtype K a b =
+    K a
+    deriving (Eq, Show)
+
+instance Functor (Flip K a) where
+    fmap f (Flip (K a)) = Flip $ K (f a)
+
+instance Arbitrary b => Arbitrary (Flip K a b) where
+    arbitrary = do
+        a <- arbitrary
+        return $ Flip $ K a
+
+flipIdentity :: Flip K Int Int -> Bool
+flipIdentity = functorIdentity
+
+flipComposition :: Flip K Int Int -> Fun Int Int -> Fun Int Int -> Bool
+flipComposition = functorComposition
+
+--- EvilGoateeConst a b
+
+data EvilGoateeConst a b =
+    GoatyConst b
+    deriving (Eq, Show)
+
+instance Functor (EvilGoateeConst a) where
+    fmap f (GoatyConst b) = GoatyConst (f b)
+
+instance (Arbitrary b) => Arbitrary (EvilGoateeConst a b) where
+    arbitrary = do
+        b <- arbitrary
+        return $ GoatyConst b
+
+goatyIdentity :: EvilGoateeConst Int Int -> Bool
+goatyIdentity = functorIdentity
+
+goatyComposition :: EvilGoateeConst Int Int -> Fun Int Int -> Fun Int Int -> Bool
+goatyComposition = functorComposition
+
+--- LiftItOut f a
+
+data LiftItOut f a =
+    LiftItOut (f a)
+    deriving (Eq, Show)
+
+instance (Functor f) => Functor (LiftItOut f) where
+    fmap g (LiftItOut fa) = LiftItOut (fmap g fa)
+
+instance (Arbitrary a) => Arbitrary (LiftItOut Maybe a) where
+    arbitrary = do
+        a <- arbitrary
+        fa <- oneof [return $ Just a,
+                     return Nothing]
+        return $ LiftItOut fa
+
+liftItOutIdentity :: LiftItOut Maybe Int -> Bool
+liftItOutIdentity = functorIdentity
+
+liftItOutComposition :: LiftItOut Maybe Int -> Fun Int Int -> Fun Int Int -> Bool
+liftItOutComposition = functorComposition
+
+--- Parappa f g a =
+
+data Parappa f g a =
+    DaWrappa (f a) (g a)
+    deriving (Eq, Show)
+
+instance (Functor f, Functor g) => Functor (Parappa f g) where
+    fmap f (DaWrappa fa ga) = DaWrappa (fmap f fa) (fmap f ga)
+
+instance (Arbitrary a) => Arbitrary (Parappa Maybe [] a) where
+    arbitrary = do
+        a <- arbitrary
+        fa <- oneof [return $ Just a,
+                     return Nothing]
+        ga <- arbitrary
+        return $ DaWrappa (fa) (ga)
+
+parappaIdentity :: Parappa Maybe [] Int -> Bool
+parappaIdentity = functorIdentity
+
+parappaComposition :: Parappa Maybe [] Int -> Fun Int Int -> Fun Int Int -> Bool
+parappaComposition = functorComposition
+
+--- IgnoreOne f g a b
+
+data IgnoreOne f g a b =
+    IgnoringSomething (f a) (g b)
+    deriving (Eq, Show)
+
+instance (Functor g) => Functor (IgnoreOne f g a) where
+  fmap f (IgnoringSomething fa gb) = IgnoringSomething fa (fmap f gb)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (IgnoreOne Maybe [] a b) where
+    arbitrary = do
+        a <- arbitrary
+        fa <- oneof [return $ Just a,
+                     return Nothing]
+        gb <- arbitrary
+        return $ IgnoringSomething fa gb
+
+ignoreOneIdentity :: IgnoreOne Maybe [] Int Int -> Bool
+ignoreOneIdentity = functorIdentity
+
+ignoreOneComposition :: IgnoreOne Maybe [] Int Int -> Fun Int Int -> Fun Int Int -> Bool
+ignoreOneComposition = functorComposition
+
+--- Notorious g o a t
+
+data Notorious g o a t =
+    Notorious (g o) (g a) (g t)
+    deriving (Eq, Show)
+
+instance (Functor g) => Functor (Notorious g o a) where
+    fmap f (Notorious go ga gt) = Notorious go ga (fmap f gt)
+
+instance (Arbitrary t) => Arbitrary (Notorious Maybe (Maybe t) (Maybe t) t) where
+    arbitrary = do
+        go <- arbitrary
+        ga <- arbitrary
+        gt <- arbitrary
+        return $ Notorious go ga gt
+
+notoriousIdentity :: Notorious Maybe (Maybe Int) (Maybe Int) Int -> Bool
+notoriousIdentity = functorIdentity
+
+notoriousComposition :: Notorious Maybe (Maybe Int) (Maybe Int) Int -> Fun Int Int -> Fun Int Int -> Bool
+notoriousComposition = functorComposition
+
+--- List a
+
+data List a =
+    Nil
+  | Cons a (List a)
+  deriving (Eq, Show)
+
+instance Functor List where
+    fmap _ Nil = Nil
+    fmap f (Cons x xs) = Cons (f x) (fmap f xs)
+
+instance (Arbitrary a) => Arbitrary (List a) where
+    arbitrary = do
+        x <- arbitrary
+        xs <- arbitrary
+        oneof [return Nil,
+               return $ Cons x xs]
+
+listIdentity :: List Int -> Bool
+listIdentity = functorIdentity
+
+listComposition :: List Int -> Fun Int Int -> Fun Int Int -> Bool
+listComposition = functorComposition
+
+-- GoatLord a
+
+data GoatLord a =
+    NoGoat
+  | OneGoat a
+  | MoreGoats (GoatLord a) (GoatLord a) (GoatLord a)
+  deriving (Eq, Show)
+
+instance Functor GoatLord where
+    fmap _ NoGoat = NoGoat
+    fmap f (OneGoat a) = OneGoat (f a)
+    fmap f (MoreGoats a b c) = MoreGoats (fmap f a) (fmap f b) (fmap f c)
+
+instance (Arbitrary a) => Arbitrary (GoatLord a) where
+    arbitrary = do
+        a <- arbitrary
+        ga <- arbitrary
+        gb <- arbitrary
+        gc <- arbitrary
+        oneof [return NoGoat,
+               return $ OneGoat a,
+               return $ MoreGoats ga gb gc]
+
+goatLordIdentity :: GoatLord Int -> Bool
+goatLordIdentity = functorIdentity
+
+goatLordComposition :: GoatLord Int -> Fun Int Int -> Fun Int Int -> Bool
+goatLordComposition = functorComposition
 
 --- Test suite driver
 
@@ -210,3 +398,59 @@ main = hspec $ do
 
         it "follows the composition law" $ do
             property quantComposition
+
+    describe "The functor for Flip K a b" $ do
+        it "follows the identity law" $ do
+            property flipIdentity
+
+        it "follows the composition law" $ do
+            property flipComposition
+
+    describe "The functor for EvilGoateeConst a b" $ do
+        it "follows the identity law" $ do
+            property goatyIdentity
+
+        it "follows the composition law" $ do
+            property goatyComposition
+
+    describe "The functor for liftItOut f a" $ do
+        it "follows the identity law" $ do
+            property liftItOutIdentity
+
+        it "follows the composition law" $ do
+            property liftItOutComposition
+
+    describe "The functor for Parappa f g a" $ do
+        it "follows the identity law" $ do
+            property parappaIdentity
+
+        it "follows the composition law" $ do
+            property parappaComposition
+
+    describe "The functor for IgnoreOne f g a b" $ do
+        it "follows the identity law" $ do
+            property ignoreOneIdentity
+
+        it "follows the composition law" $ do
+            property ignoreOneComposition
+
+    describe "The functor for Notorious g o a t" $ do
+        it "follows the identity law" $ do
+            property notoriousIdentity
+
+        it "follows the composition law" $ do
+            property notoriousComposition
+
+    describe "The functor for List a" $ do
+        it "follows the identity law" $ do
+            property listIdentity
+
+        it "follows the composition law" $ do
+            property listComposition
+
+    describe "The functor for GoatLord a" $ do
+        it "follows the identity law" $ do
+            property goatLordIdentity
+
+        it "follows the composition law" $ do
+            property goatLordComposition
