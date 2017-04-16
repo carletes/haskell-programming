cd30b8dcb07b48d77fd250c6d5c12240c5c263ed
Carlos Valiente <carlos@pepelabs.net>
2017-04-16 20:29:01 +0100

    Chapter 18.

--- 18-monad/test/exercises-chapter.hs
+++ 18-monad/test/exercises-chapter.hs
@@ -3,6 +3,7 @@
 module Main where
 
 import Prelude hiding (Left, Right)
+import Control.Monad
 
 import Test.Hspec
 import Test.Hspec.Checkers
@@ -59,6 +60,10 @@ instance (Monoid b) => Monad (PhhhbbtttEither b) where
     (Right b) >>= _ = Right b
     (Left a) >>= f = f a
 
+instance Monoid Int where
+    mempty = 0
+    x `mappend` y = x + y
+
 instance Monoid (PhhhbbtttEither Int Int) where
     mempty = Left 0
     (Right b) `mappend` _ = (Right b)
@@ -101,31 +106,141 @@ instance (Arbitrary a) => Arbitrary (Identity a) where
 instance (Eq a) => EqProp (Identity a) where
     (=-=) = eq
 
+-- List a
+
+data List a =
+    Nil
+  | Cons a (List a)
+  deriving (Eq, Show)
+
+instance Functor List where
+    fmap _ Nil = Nil
+    fmap f (Cons a as) = Cons (f a) (fmap f as)
+
+instance Monoid (List a) where
+    mempty = Nil
+    Nil `mappend` xs = xs
+    xs `mappend` Nil = xs
+    (Cons x xs) `mappend` ys = Cons x (xs `mappend` ys)
+
+instance Applicative List where
+    pure x = Cons x Nil
+    Nil <*> _ = Nil
+    _ <*> Nil = Nil
+    (Cons f fs) <*> (Cons x xs) = Cons (f x) (fs <*> xs)
+
+instance Monad List where
+    Nil >>= _ = Nil
+    (Cons x xs) >>= f = (f x) `mappend` (join $ fmap f xs)
+
+instance (Arbitrary a) => Arbitrary (List a) where
+    arbitrary = do
+        x <- arbitrary
+        xs <- arbitrary
+        oneof [return Nil,
+               return $ Cons x xs]
+
+instance (Eq a) => EqProp (List a) where
+    (=-=) = eq
+
+-- Write the following functions using the methods provided by `Monad` and `Functor`.
+
+j :: Monad m => m (m a) -> m a
+j x = do
+    y <- x
+    y
+
+l1 :: Monad m => (a -> b) -> m a -> m b
+l1 f ma = do
+    a <- ma
+    return $ f a
+
+l1' :: Monad m => (a -> b) -> m a -> m b
+l1' f ma = ma >>= (return . f)
+
+l2 :: Monad m => (a -> b -> c) -> m a -> m b -> m c
+l2 f ma mb = do
+    a <- ma
+    b <- mb
+    return $ f a b
 
 -- Test suite driver.
 
 main :: IO ()
 main = hspec $ do
     describe "Nope a" $ do
-        testBatch $ monoid (undefined :: Nope Int)
-        testBatch $ functor (undefined :: (Nope Int, (Int, Int, Int)))
-        testBatch $ applicative (undefined :: (Nope Int, (Int, Int, Int)))
-        testBatch $ monad (undefined :: (Nope Int, (Int, Int, Int)))
-        testBatch $ monadFunctor (undefined :: (Nope Int, (Int, Int)))
-        testBatch $ monadApplicative (undefined :: (Nope Int, (Int, Int)))
+        let t = NopeDotJpg :: Nope Int
+            t2 = (NopeDotJpg, (0, 0)) :: (Nope Int, (Int, Int))
+            t3 = (NopeDotJpg, (0, 0, 0)) :: (Nope Int, (Int, Int, Int))
+        testBatch $ monoid t
+        testBatch $ functor t3
+        testBatch $ applicative t3
+
+        it "implements the `Monad` typeclass" $ do
+            let f _ = NopeDotJpg
+            (NopeDotJpg >>= f) `shouldBe` NopeDotJpg
+
+        testBatch $ monad t3
+        testBatch $ monadFunctor t2
+        testBatch $ monadApplicative t2
 
     describe "PhhhbbtttEither b a" $ do
-        testBatch $ monoid (undefined :: (PhhhbbtttEither Int Int))
-        testBatch $ functor (undefined :: (PhhhbbtttEither Int Int, (Int, Int, Int)))
-        testBatch $ applicative (undefined :: (PhhhbbtttEither Int Int, (Int, Int, Int)))
-        testBatch $ monad (undefined :: (PhhhbbtttEither Int Int, (Int, Int, Int)))
-        testBatch $ monadFunctor (undefined :: (PhhhbbtttEither Int Int, (Int, Int)))
-        testBatch $ monadApplicative (undefined :: (PhhhbbtttEither Int Int, (Int, Int)))
+        let t = Left 0 :: PhhhbbtttEither Int Int
+            t2 = (t, (0, 0)) :: (PhhhbbtttEither Int Int, (Int, Int))
+            t3 = (t, (0, 0, 0)) :: (PhhhbbtttEither Int Int, (Int, Int, Int))
+        testBatch $ monoid t
+        testBatch $ functor t3
+        testBatch $ applicative t3
+
+        it "implements the `Monad` typeclass" $ do
+            let plusOne x = Left (x + 1)
+            ((Left 41) >>= plusOne) `shouldBe` (Left 42 :: PhhhbbtttEither Int Int)
+            ((Right 41) >>= plusOne) `shouldBe` (Right 41 :: PhhhbbtttEither Int Int)
+
+        testBatch $ monad t3
+        testBatch $ monadFunctor t2
+        testBatch $ monadApplicative t2
 
     describe "Identity a" $ do
-        testBatch $ monoid (undefined :: Identity Int)
-        testBatch $ functor (undefined :: (Identity Int, (Int, Int, Int)))
-        testBatch $ applicative (undefined :: (Identity Int, (Int, Int, Int)))
-        testBatch $ monad (undefined :: (Identity Int, (Int, Int, Int)))
-        testBatch $ monadFunctor (undefined :: (Identity Int, (Int, Int)))
-        testBatch $ monadApplicative (undefined :: (Identity Int, (Int, Int)))
+        let t = Identity 0
+            t2 = (t, (0, 0)) :: (Identity Int, (Int, Int))
+            t3 = (t, (0, 0, 0)) :: (Identity Int, (Int, Int, Int))
+
+        testBatch $ monoid t
+        testBatch $ functor t3
+        testBatch $ applicative t3
+
+        it "implements the `Monad` typeclass" $ do
+            let plusOne x = Identity (x + 1)
+            ((Identity 41) >>= plusOne) `shouldBe` (Identity 42 :: Identity Int)
+
+        testBatch $ monad t3
+        testBatch $ monadFunctor t2
+        testBatch $ monadApplicative t2
+
+    describe "List a" $ do
+        let t = Nil :: List Int
+            t2 = (t, (0, 0)) :: (List Int, (Int, Int))
+            t3 = (t, (0, 0, 0)) :: (List Int, (Int, Int, Int))
+
+        testBatch $ monoid t
+        testBatch $ functor t3
+        testBatch $ applicative t3
+
+        it "implements the `Monad` typeclass" $ do
+            let plusOne x = Cons (x+1) Nil :: List Int
+            (Nil >>= plusOne) `shouldBe` Nil
+            ((Cons 41 (Cons 42 (Cons 43 Nil))) >>= plusOne) `shouldBe` ((Cons 42 (Cons 43 (Cons 44 Nil))))
+
+        testBatch $ monad t3
+        testBatch $ monadFunctor t2
+        testBatch $ monadApplicative t2
+
+    describe "j" $ do
+        error "Implement `== join` with QuickCheck"
+
+    describe "l1" $ do
+        error "Implement `== liftM` with QuickCheck"
+
+    describe "l2" $ do
+        error "Implement `liftM2` with QuickCheck"
