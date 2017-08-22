6aa738cbc93437e4af3d190d152adf1f6cbdbcc7
Carlos Valiente <carlos@pepelabs.net>
2017-08-22 15:52:48 +0100

    Chapter 22 (work in progress).

---
+++ 22-reader/test/exercises-chapter.hs
@@ -0,0 +1,17 @@
+module Main where
+
+import System.Exit
+
+-- Some sample data.
+
+x :: [Integer]
+x = [1, 2, 3]
+
+y :: [Integer]
+y = [4, 5, 6]
+
+z :: [Integer]
+z = [7, 8, 9]
+
+main :: IO ()
+main = exitFailure
