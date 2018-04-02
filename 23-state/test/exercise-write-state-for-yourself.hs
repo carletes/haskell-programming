1c14ee940e01237b24ec3285f2520ee4915cf560
Carlos Valiente <carlos@pepelabs.net>
2018-04-02 20:31:38 +0100

    Chapter 23.

--- 23-state/test/exercise-write-state-for-yourself.hs
+++ 23-state/test/exercise-write-state-for-yourself.hs
@@ -1,24 +1,43 @@
 module Main where
 
+import Control.Applicative
 import Test.Hspec
 import Test.Hspec.Checkers
 import Test.QuickCheck
 import Test.QuickCheck.Checkers
 import Test.QuickCheck.Classes
-import Test.QuickCheck.Function
 
 
 -- Our `State` type.
 
 newtype Moi s a =
-    Moi { runMoi :: s -> (a, s)}
+    Moi { runMoi :: s -> (a, s) }
 
 -- Functor instance.
 
 instance Functor (Moi s) where
     fmap f (Moi sa) = Moi (\s -> (f $ fst (sa s), s))
 
+-- Applicative instance.
+
+instance Applicative (Moi s) where
+    pure a = Moi (\s -> (a, s))
+    (Moi sab) <*> (Moi sa) = Moi (\s -> (fst (sab s) (fst (sa s)), s))
+
 -- Test suite helpers.
+
+instance (Monoid a) => Monoid (Moi s a) where
+    mempty = Moi (\s -> (mempty, s))
+    (Moi sa) `mappend` (Moi sb) = Moi (\s -> (fst (sa s) `mappend` fst (sb s), s))
+
+instance (CoArbitrary s, Arbitrary a, Arbitrary s) => Arbitrary (Moi s a) where
+    arbitrary = fmap Moi arbitrary
+
+instance Show (Moi s a) where
+  show _ = "<Moi s a>"
+
+instance (Arbitrary s, Show s, EqProp s, EqProp a) => EqProp (Moi s a) where
+    (Moi sa) =-= (Moi sb) = property $ liftA2 (=-=) sa sb
 
 -- Test suite driver.
 
@@ -26,3 +45,4 @@ main :: IO ()
 main = hspec $
     describe "Moi s a" $ do
       testBatch $ functor (undefined :: (Moi String Int, (Int, Int, Int)))
+      testBatch $ applicative (undefined :: (Moi String String, (String, String, String)))
